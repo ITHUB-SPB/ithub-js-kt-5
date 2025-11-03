6bc0d99694dddccdf5b5f8ae88ddbbf3e9663cb5
Lex Trofimov <daslef93@gmail.com>
2025-11-03 16:29:00 +0300

    обновлено README, удалены неактуальные файлы

--- src/task3.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {any[]} initialArray - Исходный массив
- * @returns {undefined} 
- * 
- * @description Разворот массива на месте с выводом в консоль
- */
-export function reverseArray(initialArray) {
-    return
-}
