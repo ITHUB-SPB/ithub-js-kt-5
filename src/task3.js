b83e06c74632d97e238b61e0e14240f08ea1e1e2
Gagaktus <161711593+Gagaktus@users.noreply.github.com>
2025-11-17 14:32:09 +0300

    Update task3.js

--- src/task3.js
+++ src/task3.js
@@ -2,13 +2,11 @@ export function formatPhones(phones) {
     for (let i = 0; i < phones.length; i++) {
         const phone = phones[i];
         
-        // Убираем все нецифровые символы кроме +
         const cleanPhone = phone.replace(/[^\d+]/g, '');
         
         const plusIndex = cleanPhone.indexOf('+');
         const digits = cleanPhone.slice(plusIndex + 1);
         
-        // Определяем длину кода страны (1-3 цифры)
         let codeLength = 1;
         if (digits.startsWith('375')) codeLength = 3;
         else if (digits.startsWith('57') || digits.startsWith('37')) codeLength = 2;
@@ -16,7 +14,6 @@ export function formatPhones(phones) {
         const countryCode = '+' + digits.slice(0, codeLength);
         const numberPart = digits.slice(codeLength);
         
-        // Разбиваем на группы: 3-3-2-2
         const group1 = numberPart.slice(0, 3);
         const group2 = numberPart.slice(3, 6);
         const group3 = numberPart.slice(6, 8);
