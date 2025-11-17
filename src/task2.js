da67837d9250c1769471c6548f832eddd623bbb0
Konstantin Barbarich <barbarickonstantin@gmail.com>
2025-11-17 13:38:52 +0300

    task 1 & 2 done

--- src/task2.js
+++ src/task2.js
@@ -11,5 +11,27 @@
  * logDate(new Date()) // Сегодня вторник
  */
 export function logDay(date) {
-    
+    let days = [
+        'Воскресенье',
+        'Понедельник',
+        'Вторник',
+        'Среда',
+        'Четверг',
+        'Пятница',
+        'Суббота',
+    ]
+
+    let todaydays = [
+        'Сегодня воскресенье',
+        'Сегодня понедельник',
+        'Сегодня вторник',
+        'Сегодня среда',
+        'Сегодня четверг',
+        'Сегодня пятница',
+        'Сегодня суббота',
+    ]
+    let today = new Date()
+    if (date.toDateString() === today.toDateString())
+        console.log(todaydays[today.getDay()])
+    else console.log(days[date.getDay()])
 }
