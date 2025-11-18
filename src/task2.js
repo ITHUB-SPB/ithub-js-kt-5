c7c41426cb203941796271b1b79c8ab6edc04349
Asya Belshova <asyaa@disroot.org>
2025-11-18 21:40:05 +0300

    format using prettier

--- src/task2.js
+++ src/task2.js
@@ -11,14 +11,17 @@
  * logDate(new Date()) // Сегодня вторник
  */
 export function logDay(date) {
-	const weekday = date.toLocaleDateString('ru', {weekday: 'long'})
-	const now = new Date()
-	const result = (now.getFullYear() === date.getFullYear() &&
-                    now.getMonth() === date.getMonth() &&
-				    now.getDate() === date.getDate()) ? "сегодня " + weekday : weekday
+    const weekday = date.toLocaleDateString('ru', { weekday: 'long' })
+    const now = new Date()
+    const result =
+        now.getFullYear() === date.getFullYear() &&
+        now.getMonth() === date.getMonth() &&
+        now.getDate() === date.getDate()
+            ? 'сегодня ' + weekday
+            : weekday
     console.log(capitalizeFirstLetter(result))
 }
 
 function capitalizeFirstLetter(val) {
-    return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+    return String(val).charAt(0).toUpperCase() + String(val).slice(1)
 }
--- src/task3.js
+++ src/task3.js
@@ -5,7 +5,7 @@
  * @description Функция принимает на вход дату в строковом
  * отображении и возвращает информацию о ее формате. Постарайтесь
  * использовать регулярные выражения и методы `match` либо `test`.
- * 
+ *
  * @throws Ошибка при некорректном формате даты
  *
  * @example
@@ -14,23 +14,23 @@
  * getDateFormat("18.10/22")   // Error: Некорректный формат даты
  */
 export function getDateFormat(date) {
-	const formats = {
-		"европейский": /^(\d{4})\/(\d{2})\/(\d{2})$/,
-		"российский": /^(\d{2})\.(\d{2})\.(\d{4})$/
-	};
+    const formats = {
+        европейский: /^(\d{4})\/(\d{2})\/(\d{2})$/,
+        российский: /^(\d{2})\.(\d{2})\.(\d{4})$/,
+    }
 
-	for (const [format, regex] of Object.entries(formats)) {
-		const match = date.match(regex)
-		if (match) {
-			const month = parseInt(match[2], 10)
+    for (const [format, regex] of Object.entries(formats)) {
+        const match = date.match(regex)
+        if (match) {
+            const month = parseInt(match[2], 10)
 
-			if (month < 1 || month > 12) {
-				throw new Error("Некорректный формат даты")
-			}
+            if (month < 1 || month > 12) {
+                throw new Error('Некорректный формат даты')
+            }
 
-			return `Формат даты ${format}`
-		}
-	}
+            return `Формат даты ${format}`
+        }
+    }
 
-	throw new Error("Некорректный формат даты")
+    throw new Error('Некорректный формат даты')
 }
