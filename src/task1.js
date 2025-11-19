76b589d2946fa8f6608ca840f588566e01bc8596
Твоё Имя <you@example.com>
2025-11-19 16:18:07 +0300

    hhh

--- src/task1.js
+++ src/task1.js
@@ -9,5 +9,7 @@
  * console.log(getUnixHours()) // 489780
  */
 export function getUnixHours() {
-    return 0
-}
+    const msSince1970 = Date.now();
+    const hours = msSince1970 / (1000 * 60 * 60);
+    return Math.round(hours);
+  }
