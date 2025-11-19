a80045b5010ad7094ee333ff586be9f4e038e2a3
Kiprian Belshov <belshovka24@spb.ithub.ru>
2025-11-19 13:45:15 +0300

    add tests for task5

--- tests/task5.test.js
+++ tests/task5.test.js
@@ -6,9 +6,9 @@ describe("функция secondOccurenceInterval", () => {
   test("[0.5] случай отсутствие совпадений", ({ annotate }) => {
     annotate(0.5)
 
-    const text = '' // TODO
-    const substring = '' // TODO
-    const expected = {} // TODO
+    const text = 'foo'
+    const substring = 'bar'
+    const expected = null
 
     assert.deepEqual(secondOccurenceInterval(substring, text), expected)
   })
@@ -16,9 +16,9 @@ describe("функция secondOccurenceInterval", () => {
   test("[0.5] одно совпадение", ({ annotate }) => {
     annotate(0.5)
 
-    const text = '' // TODO
-    const substring = '' // TODO
-    const expected = {} // TODO
+    const text = 'foobar'
+    const substring = 'foo'
+    const expected = null
 
     assert.deepEqual(secondOccurenceInterval(substring, text), expected)
   })
@@ -26,9 +26,9 @@ describe("функция secondOccurenceInterval", () => {
   test("[0.5] два совпадения", ({ annotate }) => {
     annotate(0.5)
 
-    const text = '' // TODO
-    const substring = '' // TODO
-    const expected = {} // TODO
+    const text = 'foobarfoo'
+    const substring = 'foo'
+    const expected = {from: 6, to: 8}
 
     assert.deepEqual(secondOccurenceInterval(substring, text), expected)
   })
@@ -36,9 +36,9 @@ describe("функция secondOccurenceInterval", () => {
   test("[0.5] более двух совпадений", ({ annotate }) => {
     annotate(0.5)
 
-    const text = '' // TODO
-    const substring = '' // TODO
-    const expected = {} // TODO
+    const text = 'foofoofoo'
+    const substring = 'foo'
+    const expected = {from: 3, to: 5}
 
     assert.deepEqual(secondOccurenceInterval(substring, text), expected)
   })
