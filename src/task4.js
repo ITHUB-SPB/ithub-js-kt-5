1a37e074c5692b5b13d196bdbbd5ffa8396cb6aa
mquwmuy <koroteeva.mai@yandex.ru>
2025-11-21 00:03:12 +0300

    Update task4.js

--- src/task4.js
+++ src/task4.js
@@ -4,16 +4,31 @@
  *
  * @description Функция принимает на вход массив строк, потенциально
  * являющихся адресами эл. почты и возвращает объект с информацией о
- * валидности каждого из них. Критерии правильной почты возьмите из 
- * исходного кода (не устанавливая ее, а скопировав нужную строку!) 
+ * валидности каждого из них. Критерии правильной почты возьмите из
+ * исходного кода (не устанавливая ее, а скопировав нужную строку!)
  * любой из популярных библиотек для валидации (zod, valibot).
  *
  * @example
  * const emails = ['top@dot.com', 'abra@gmailcom', 'pot.mail.ru', 'grek@pro.pro']
  * { top@dot.com: true, abra@gmailcom: false , pot.mail.ru: false , grek@pro.pro: true }
  */
+
+
 export function validateEmails(emailArray) {
-    const regexEmail = /^[^@]+@[^@]+\.[^@]+$/
-    
-    return
+    const emailRegex = /^\w+@\w+\.\w+$/i          //любой регистр
+    return emailArray.reduce((acc, email) => {
+        acc[email] = emailRegex.test(email)
+        return acc
+    }, {})
 }
+
+
+// export function validateEmails(emailArray) {
+//     const emailRegex = /^\w+@\w+\.\w+$/
+
+//     // Используем метод map для проверки каждой электронной почты
+//     const resultEntries = emailArray.map(email => ([email, emailRegex.test(email)]))
+    
+//     // Преобразуем массив пар ключ-значение в объект
+//     return Object.fromEntries(resultEntries);
+// }
