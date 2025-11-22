1f9ddfc15f17828980a453e432f003d69fb07a2f
spaeeell <spaelbro@gmail.com>
2025-11-22 12:42:54 +0300

    Update task4.js

--- src/task4.js
+++ src/task4.js
@@ -10,13 +10,13 @@
  * console.log(result) // { login: "example", domain: "gmail", zone: "com" }
  */
 export function splitEmail(email) {
-    // Регулярное выражение для разбора email
+    
     const regex = /^(.+)@([^.]+)(..+)$/;
     const match = email.match(regex);
 
-        const login = match[1]; // Логин
-        const domain = match[2]; // Домен
-        const zone = match[3].substring(1); // Доменная зона (без точки)
+        const login = match[1];
+        const domain = match[2]; 
+        const zone = match[3].substring(1);
 
         return { login, domain, zone };
     
